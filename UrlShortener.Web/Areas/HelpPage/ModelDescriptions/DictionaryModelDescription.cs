45a643aef0f7271c8cedd77ec39e9095d5da87c4
pikadanx <daniel@pikadan.com>
2015-06-12 23:37:21 -0700

    Initial Commit WebApi Project from VS Wizard

---
+++ UrlShortener.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace UrlShortener.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ UrlShortener.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UrlShortener.Web.WebApiApplication" Language="C#" %>
