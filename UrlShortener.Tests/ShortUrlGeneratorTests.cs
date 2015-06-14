684482ff730e591f1dea716b81422a0914108edd
pikadanx <daniel@pikadan.com>
2015-06-14 15:46:25 -0700

    implemented ShortUrlGenerator with tests

---
+++ UrlShortener.Tests/ShortUrlGeneratorTests.cs
@@ -0,0 +1,81 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using UrlShortener.DataAccess;
+
+namespace UrlShortener.Tests
+{
+    [TestClass]
+    public class ShortUrlGeneratorTests
+    {
+        private ShortUrlGenerator shortUrlGenerator;
+        private Mock<IShortUrlDataStore> mockDataStore;
+
+        [TestInitialize]
+        public void Init()
+        {
+            mockDataStore = new Mock<IShortUrlDataStore>();
+            shortUrlGenerator = new ShortUrlGenerator(mockDataStore.Object);
+        }
+
+        [TestMethod]
+        public async Task GetNextShortUrlHash_CallsGetNextShortUrlIdOnDataStore()
+        {
+            mockDataStore.Setup(store => store.GetNextShortUrlId()).ReturnsAsync(0).Verifiable("Expected IShortUrlDataStore.GetNextShortUrlId() to be called.");
+
+            await shortUrlGenerator.GetNextShortUrlHash();
+
+            mockDataStore.Verify();
+        }
+
+        [TestMethod]
+        public async Task GetNextShortUrlHash_Returns0_WhenIdIs0()
+        {
+            mockDataStore.Setup(store => store.GetNextShortUrlId()).ReturnsAsync(0);
+
+            var hash = await shortUrlGenerator.GetNextShortUrlHash();
+
+            Assert.AreEqual("0", hash);
+        }
+
+        [TestMethod]
+        public async Task GetNextShortUrlHash_ReturnsTwoCharacters_WhenIdIsOneOverCharacterSetCount()
+        {
+            mockDataStore.Setup(store => store.GetNextShortUrlId()).ReturnsAsync(65);
+
+            var hash = await shortUrlGenerator.GetNextShortUrlHash();
+
+            Assert.AreEqual("10", hash);
+        }
+
+        [TestMethod]
+        public async Task GetNextShortUrlHash_ReturnsValue_WhenIdIsMaxValue()
+        {
+            mockDataStore.Setup(store => store.GetNextShortUrlId()).ReturnsAsync(long.MaxValue);
+
+            var hash = await shortUrlGenerator.GetNextShortUrlHash();
+
+            Assert.AreEqual("6TkEooEkxO7", hash);
+        }
+
+        [TestMethod]
+        public async Task GetNextShortUrlHash_ReturnsWithLeadingTilde_WhenIdIsNegativeValue()
+        {
+            mockDataStore.Setup(store => store.GetNextShortUrlId()).ReturnsAsync(-1);
+
+            var hash = await shortUrlGenerator.GetNextShortUrlHash();
+
+            Assert.AreEqual("~1", hash);
+        }
+
+        [TestMethod]
+        public async Task GetNextShortUrlHash_ReturnsTilde_WhenIdIsMinValue()
+        {
+            mockDataStore.Setup(store => store.GetNextShortUrlId()).ReturnsAsync(long.MinValue);
+
+            var hash = await shortUrlGenerator.GetNextShortUrlHash();
+
+            Assert.AreEqual("~", hash);
+        }
+    }
+}
---
+++ UrlShortener/ShortUrlGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using UrlShortener.DataAccess;
+
+namespace UrlShortener
+{
+    public class ShortUrlGenerator : IShortUrlGenerator
+    {
+        private readonly IShortUrlDataStore dataStore;
+        private const string UriUnreservedCharacters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-._~"; // from https://tools.ietf.org/html/rfc3986#section-2.3
+        private static readonly int Base = UriUnreservedCharacters.Length - 1; // reserve last char for negative numbers
+
+        public ShortUrlGenerator(IShortUrlDataStore dataStore)
+        {
+            this.dataStore = dataStore;
+        }
+
+        public async Task<string> GetNextShortUrlHash()
+        {
+            var id = await dataStore.GetNextShortUrlId();
+
+            return EncodeId(id);
+        }
+
+        private static string EncodeId(long id)
+        {
+            if (id == 0)
+            {
+                return UriUnreservedCharacters[0].ToString();
+            }
+
+            bool negative = id < 0;
+            var sb = new StringBuilder();
+
+            if (negative)
+            {
+                sb.Append(UriUnreservedCharacters[Base]);
+                id *= -1;
+            }
+
+            while (id > 0)
+            {
+                sb.Insert(negative ? 1 : 0, UriUnreservedCharacters[(int) (id%Base)]);
+                id = id / Base;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
