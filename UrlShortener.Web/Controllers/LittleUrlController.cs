d53e4fdd1200c2ec9998e36df7bd790b9973d626
pikadanx <daniel@pikadan.com>
2015-06-14 21:01:22 -0700

    created LittleUrlController

---
+++ UrlShortener.Web/Controllers/LittleUrlController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+namespace UrlShortener.Web.Controllers
+{
+    public class LittleUrlController : ApiController
+    {
+        private readonly IUrlShortener urlShortener;
+        private readonly IShortUrlResolver shortUrlResolver;
+
+        public LittleUrlController(IUrlShortener urlShortener, IShortUrlResolver shortUrlResolver)
+        {
+            this.urlShortener = urlShortener;
+            this.shortUrlResolver = shortUrlResolver;
+        }
+    }
+}
