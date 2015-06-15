d6a8b4110b4a491aae6138b55f247c2f103ae948
pikadanx <daniel@pikadan.com>
2015-06-15 00:44:59 -0700

    updated serialization settings

--- UrlShortener.Web/App_Start/WebApiConfig.cs
+++ UrlShortener.Web/App_Start/WebApiConfig.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace UrlShortener.Web
 {
@@ -10,6 +9,8 @@ namespace UrlShortener.Web
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            RemoveXmlSerialization(config);
+            SetupJsonSerializerSettings(config.Formatters.JsonFormatter.SerializerSettings);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -19,6 +20,17 @@ namespace UrlShortener.Web
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+        }
+
+        private static void RemoveXmlSerialization(HttpConfiguration config)
+        {
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+        }
+
+        private static void SetupJsonSerializerSettings(JsonSerializerSettings jsonSerializerSetttings)
+        {
+            jsonSerializerSetttings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            jsonSerializerSetttings.NullValueHandling = NullValueHandling.Ignore;
         }
     }
 }
