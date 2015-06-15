eee41e0e6ce9e3514716e879b491b17d568329d0
pikadanx <daniel@pikadan.com>
2015-06-14 21:19:08 -0700

    added Unity IoC container for LittleController

---
+++ UrlShortener.Web/App_Start/UnityConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Http;
+using Microsoft.Practices.Unity;
+using UrlShortener.DataAccess;
+using UrlShortener.Web.Controllers;
+
+namespace UrlShortener.Web
+{
+    public class UnityConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var container = new UnityContainer();
+
+            const string shortUrlDataStoreSingletonName = "ShortUrlDataStore-Singleton";
+            const string shortUrlGeneratorSingletonName = "ShortUrlGenerator-Singleton";
+            const string urlShortenerSingletonName = "UrlShortener-Singleton";
+            const string shortUrlResolverSingletonName = "ShortUrlResolver-Singleton";
+
+            container.RegisterType<IShortUrlDataStore, RedisShortUrlDataStore>(shortUrlDataStoreSingletonName,
+                new ExternallyControlledLifetimeManager());
+            container.RegisterType<IShortUrlGenerator, ShortUrlGenerator>(shortUrlGeneratorSingletonName,
+                new ExternallyControlledLifetimeManager(),
+                new InjectionConstructor(new ResolvedParameter<IShortUrlDataStore>(shortUrlDataStoreSingletonName)));
+            container.RegisterType<IUrlShortener, UrlShortener>(urlShortenerSingletonName,
+                new ExternallyControlledLifetimeManager(),
+                new InjectionConstructor(new ResolvedParameter<IShortUrlDataStore>(shortUrlDataStoreSingletonName),
+                    new ResolvedParameter<IShortUrlGenerator>(shortUrlGeneratorSingletonName)));
+            container.RegisterType<IShortUrlResolver, ShortUrlResolver>(shortUrlResolverSingletonName,
+                new ExternallyControlledLifetimeManager(),
+                new InjectionConstructor(new ResolvedParameter<IShortUrlDataStore>(shortUrlDataStoreSingletonName)));
+            container.RegisterType<LittleUrlController>(
+                new InjectionConstructor(new ResolvedParameter<IUrlShortener>(urlShortenerSingletonName),
+                    new ResolvedParameter<IShortUrlResolver>(shortUrlResolverSingletonName)));
+
+            config.DependencyResolver = new UnityResolver(container);
+        }
+    }
+}
--- UrlShortener.Web/Global.asax.cs
+++ UrlShortener.Web/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -9,12 +6,13 @@ using System.Web.Routing;
 
 namespace UrlShortener.Web
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configure(UnityConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
