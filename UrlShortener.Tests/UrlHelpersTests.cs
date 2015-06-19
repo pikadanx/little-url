24270789b284b6e4ce630401d73dc40b8a92e710
pikadanx <daniel@pikadan.com>
2015-06-18 23:43:05 -0700

    added UrlHelpers

---
+++ UrlShortener.Tests/UrlHelpersTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UrlShortener.Tests
+{
+    [TestClass]
+    public class UrlHelpersTests
+    {
+        [TestMethod]
+        public void TryGetUrlKeyFromShortUrl_ReturnsTrueAndKey()
+        {
+            string result;
+            Assert.IsTrue(UrlHelpers.TryGetUrlKeyFromShortUrl("http://example.com/foo", out result));
+
+            Assert.AreEqual("foo", result);
+        }
+
+        [TestMethod]
+        public void TryGetUrlKeyFromShortUrl_ReturnsTrueAndKey_WhenSlashIsAfterKey()
+        {
+            string result;
+            Assert.IsTrue(UrlHelpers.TryGetUrlKeyFromShortUrl("http://example.com/foo/", out result));
+
+            Assert.AreEqual("foo", result);
+        }
+
+        [TestMethod]
+        public void TryGetUrlKeyFromShortUrl_ReturnsTrueAndKey_WhenQuestingMarkIsAfterKey()
+        {
+            string result;
+            Assert.IsTrue(UrlHelpers.TryGetUrlKeyFromShortUrl("http://example.com/foo?", out result));
+
+            Assert.AreEqual("foo", result);
+        }
+
+        [TestMethod]
+        public void TryGetUrlKeyFromShortUrl_ReturnsTrueAndKey_WhenSchemeIsMissing()
+        {
+            string result;
+            Assert.IsTrue(UrlHelpers.TryGetUrlKeyFromShortUrl("example.com/foo?", out result));
+
+            Assert.AreEqual("foo", result);
+        }
+
+        [TestMethod]
+        public void TryGetUrlKeyFromShortUrl_ReturnsTrueAndKey_WhenBeginsWithSlashSlash()
+        {
+            string result;
+            Assert.IsTrue(UrlHelpers.TryGetUrlKeyFromShortUrl("//example.com/foo?", out result));
+
+            Assert.AreEqual("foo", result);
+        }
+
+        [TestMethod]
+        public void TryGetUrlKeyFromShortUrl_ReturnsFalse_WhenKeyNotInPath()
+        {
+            string result;
+            Assert.IsFalse(UrlHelpers.TryGetUrlKeyFromShortUrl("http://example.com", out result));
+        }
+
+        [TestMethod]
+        public void TryGetUrlKeyFromShortUrl_ReturnsFalse_WhenKeyNotInPathWithQuery()
+        {
+            string result;
+            Assert.IsFalse(UrlHelpers.TryGetUrlKeyFromShortUrl("http://example.com?key=value", out result));
+        }
+
+        [TestMethod]
+        public void GetAsCanonialHttpUrl_ReturnsUrlAsIs_WhenUrlHasHttpScheme()
+        {
+            var result = UrlHelpers.GetAsCanonialHttpUrl("http://example.com");
+
+            Assert.AreEqual("http://example.com", result);
+        }
+
+        [TestMethod]
+        public void GetAsCanonialHttpUrl_ReturnsUrlAsIs_WhenUrlHasHttpsScheme()
+        {
+            var result = UrlHelpers.GetAsCanonialHttpUrl("https://example.com");
+
+            Assert.AreEqual("https://example.com", result);
+        }
+
+        [TestMethod]
+        public void GetAsCanonialHttpUrl_ReturnsUrlWithHttpScheme_WhenUrlHasNoScheme()
+        {
+            var result = UrlHelpers.GetAsCanonialHttpUrl("example.com");
+
+            Assert.AreEqual("http://example.com", result);
+        }
+
+        [TestMethod]
+        public void GetAsCanonialHttpUrl_ReturnsUrlWithHttpScheme_WhenUrlHasNoSchemeAndStartsWithSlashSlash()
+        {
+            var result = UrlHelpers.GetAsCanonialHttpUrl("//example.com");
+
+            Assert.AreEqual("http://example.com", result);
+        }
+
+        [TestMethod]
+        public void GetAsCanonialHttpUrl_ReturnsUrlWithHttpScheme_WhenUrlHasNoSchemeAndStartsWithHttp()
+        {
+            var result = UrlHelpers.GetAsCanonialHttpUrl("httpexample.com");
+
+            Assert.AreEqual("http://httpexample.com", result);
+        }
+    }
+}
---
+++ UrlShortener/UrlHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace UrlShortener
+{
+    public static class UrlHelpers
+    {
+        public static bool TryGetUrlKeyFromShortUrl(string shortUrl, out string urlKey)
+        {
+            shortUrl = GetAsCanonialHttpUrl(shortUrl);
+
+            Uri url;
+            if (Uri.TryCreate(shortUrl, UriKind.Absolute, out url))
+            {
+                urlKey = url.AbsolutePath.Split('/')[1];
+                return !String.IsNullOrEmpty(urlKey);
+            }
+
+            urlKey = String.Empty;
+            return false;
+        }
+
+        public static string GetAsCanonialHttpUrl(string url)
+        {
+            if (url.StartsWith("//", StringComparison.OrdinalIgnoreCase))
+            {
+                url = "http:" + url;
+            }
+            else if (!(url.StartsWith("http:", StringComparison.OrdinalIgnoreCase) ||
+                       url.StartsWith("https:", StringComparison.OrdinalIgnoreCase)))
+            {
+                url = "http://" + url;
+            }
+            return url;
+        }
+    }
+}
