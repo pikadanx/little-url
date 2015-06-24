313d0fd8b8aa8e7a73f804e015c15b64d13385de
pikadanx <daniel@pikadan.com>
2015-06-24 01:17:52 -0700

    removed modernizr and respond.js

--- UrlShortener.Web/App_Start/BundleConfig.cs
+++ UrlShortener.Web/App_Start/BundleConfig.cs
@@ -11,14 +11,8 @@ namespace UrlShortener.Web
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
